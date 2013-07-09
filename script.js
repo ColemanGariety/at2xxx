271f26a461ba764af6ab3141ea24ab801fa7816b
Jackson Gariety <personal@jacksongariety.com>
2013-07-08 21:15:08 -0700

    First time touching the HTML5 fulslcreen API. (not as esy to use as the rest of HTML5)

--- script.js
+++ script.js
@@ -5,7 +5,79 @@ var randomDate = function(start, end) {
     	year = value.getUTCFullYear(),
     	date = '0' + value.getDate()
 	return year + '-' + modMonth.slice(-2) + "-" + date.slice(-2);
-}
+};
+
+(function() {
+	var fullScreenApi = { 
+			supportsFullScreen: false,
+			isFullScreen: function() { return false; }, 
+			requestFullScreen: function() {}, 
+			cancelFullScreen: function() {},
+			fullScreenEventName: '',
+			prefix: ''
+		},
+		browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+	
+	// check for native support
+	if (typeof document.cancelFullScreen != 'undefined') {
+		fullScreenApi.supportsFullScreen = true;
+	} else {	 
+		// check for fullscreen support by vendor prefix
+		for (var i = 0, il = browserPrefixes.length; i < il; i++ ) {
+			fullScreenApi.prefix = browserPrefixes[i];
+			
+			if (typeof document[fullScreenApi.prefix + 'CancelFullScreen' ] != 'undefined' ) {
+				fullScreenApi.supportsFullScreen = true;
+				
+				break;
+			}
+		}
+	}
+	
+	// update methods to do something useful
+	if (fullScreenApi.supportsFullScreen) {
+		fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+		
+		fullScreenApi.isFullScreen = function() {
+			switch (this.prefix) {	
+				case '':
+					return document.fullScreen;
+				case 'webkit':
+					return document.webkitIsFullScreen;
+				default:
+					return document[this.prefix + 'FullScreen'];
+			}
+		}
+		fullScreenApi.requestFullScreen = function(el) {
+			return (this.prefix === '') ? el.requestFullScreen() : el[this.prefix + 'RequestFullScreen']();
+		}
+		fullScreenApi.cancelFullScreen = function(el) {
+			return (this.prefix === '') ? document.cancelFullScreen() : document[this.prefix + 'CancelFullScreen']();
+		}		
+	}
+
+	// jQuery plugin
+	if (typeof jQuery != 'undefined') {
+		jQuery.fn.requestFullScreen = function() {
+	
+			return this.each(function() {
+				var el = jQuery(this);
+				if (fullScreenApi.supportsFullScreen) {
+					fullScreenApi.requestFullScreen(el);
+				}
+			});
+		};
+	}
+
+	// export api
+	window.fullScreenApi = fullScreenApi;	
+})();
+
+$(document).keydown(function(event) {
+  if (event.which == 70) {
+    window.fullScreenApi.requestFullScreen($("html").get(0))
+  }
+});
 
 var images = [],
     readyForMore = true,
@@ -16,7 +88,7 @@ var append = function() {
   var photo = images[0]
   
   if (window.location.hash == "#nsfw") {
-      var str = "<img src=\"http://i.imgur.com/" + photo.hash + photo.ext + "\" onload=\"nextImage()\" class=\"hidden\">"
+      var str = "<img src=\"http://i.imgur.com/" + photo.hash + "h" + photo.ext + "\" onload=\"nextImage()\" class=\"hidden\">"
   } else {
       var str = "<img src=\"http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_b.jpg\" onload=\"nextImage()\" class=\"hidden\">"
   }
